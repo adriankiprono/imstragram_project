556247c373aa3277830884fca7421ecb7b5687f7
adriankiprono <tuimuradrian6@gmail.com>
2020-01-01 17:59:28 +0300

    add image tag

--- photos/urls.py
+++ photos/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 
 from . import views
@@ -7,3 +8,5 @@ urlpatterns=[
     url(r'^$',views.home,name = 'home'),
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
