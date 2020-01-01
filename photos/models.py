0e5de9878c0311916c7d0472e02163321e35af3e
adriankiprono <tuimuradrian6@gmail.com>
2020-01-01 16:20:33 +0300

    add image and profile model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.TextField(max_length=40)
+    bio = models.CharField(max_length= 100)
+    def __str__(self):
+        return self.username
+
+class Image(models.Model):
+    image_name= models.CharField(max_length=60)
+    profile =models.ForeignKey('Profile',on_delete=models.CASCADE,)
+    def __str__(self):
+        return self.image_name
+
+
