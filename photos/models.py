62d48786569ae10b2357d609a5562c8dfe47edb0
adriankiprono <tuimuradrian6@gmail.com>
2020-01-07 10:47:43 +0300

    pip install django-tinymce

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,16 +9,12 @@ class Profile(models.Model):
     def __str__(self):
         return self.username
     
-    
-
-    
-
 class Image(models.Model):
     name= models.CharField(max_length=60)
-    profile =models.ForeignKey('Profile',on_delete=models.CASCADE,)
+    profile =models.ForeignKey(User,on_delete=models.CASCADE,)
     image= models.ImageField(upload_to= 'images/',default='image')
     def __str__(self):
-        return self.image_name
+        return self.name
 
     @classmethod
     def search_by_name(cls,search_term):
