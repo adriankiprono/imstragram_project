517b1a33362c281c949a4f4e4de61f794840f9ff
adriankiprono <tuimuradrian6@gmail.com>
2020-01-08 13:33:45 +0300

    11  deploy

--- instargram/settings.py
+++ instargram/settings.py
@@ -81,7 +81,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,"templates")
+            
+            os.path.join(BASE_DIR ,"templates/registration"),
+            os.path.join(BASE_DIR ,"templates")
         ],
 
         'APP_DIRS': True,
