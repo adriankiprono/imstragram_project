59c51da91eba48a5a38d457a67a3d85ffd98c7e6
adriankiprono <tuimuradrian6@gmail.com>
2020-01-08 12:56:58 +0300

    10 deploymennt

--- instargram/settings.py
+++ instargram/settings.py
@@ -78,7 +78,10 @@ ROOT_URLCONF = 'instargram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
